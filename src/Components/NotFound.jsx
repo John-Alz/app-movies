2c3be83ccab243839e994a89000cd3844e29c877
John-Alz <jairoanngelll@gmail.com>
2022-12-01 18:50:30 -0500

    notfound

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -9,9 +9,12 @@ export default function NotFound() {
         <h2 className='text-center mt-20 font-bold text-3xl text-purple-600'> ¡Oh! La página no se puede encontrar...</h2>
         <h3 className='text-center mt-10 font-bold text-2xl text-purple-400'>El enlace puede ser incorrecto...</h3>
         <h4 className='text-center mt-2 font-bold text-2xl text-purple-400'>o la pagina fue borrada</h4>
+        <div className='ml-24'>
         <div className=' ml-64 mt-20'>
         <Link to='/listado'><button className='	ml-96 bg-purple-800 text-white font-bold py-2 px-4 '>Regresa</button> </Link>
         </div>
+        </div>
+        
           
       </div>
   </div>
